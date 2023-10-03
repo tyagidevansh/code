220f046ab7ea09a264432683d16a79495704248e
tyagidevansh <devanshtyagi0607@gmail.com>
2023-10-03 22:23:31 +0530

    usual

---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Complex {
+private:
+    float real;
+    float imag;
+public:
+    Complex() : real(0.0), imag(0.) {}
+
+    Complex (float value) : real(value), imag(value) {}
+
+    Complex (float real, float imag) : real(real), imag(imag) {}
+
+    Complex operator+(const Complex& other) {
+        Complex result;
+        result.real = real + other.real;
+        result.imag = imag + other.imag;
+        return result;
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main(){
+    Complex num1;
+    Complex num2(3.0);
+    Complex num3(1.5, 2.5);
+    Complex sum = num2+ num3;
+
+    num2.display();
+    num3.display();
+    sum.display();
+
+    return 0;
+}
