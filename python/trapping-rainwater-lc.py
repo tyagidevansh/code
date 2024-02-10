a6be2cc8e8f11fe8bde76c7cb36d1bc256503107
tyagidevansh <devanshtyagi0607@gmail.com>
2024-02-10 23:34:34 +0530

    usual

--- python/trapping-rainwater-lc.py
+++ python/trapping-rainwater-lc.py
@@ -1,20 +1,15 @@
 def trap(height: list[int]) -> int:
-        i, j = 0, 1
-        temp = []
-        res = 0
-        while j < len(height) - 1:
-            diff = abs(height[j] - height[i])
-            if diff != 0:
-                while j > i:
-                    area = abs(height[i+1] - height[i])
-                    res += area
-                    i+=1     
-                j += 1   
-            else:
-                i+=1
-        
-        return res
-
-h = [0,1,0,2,1,0,1,3,2,1,2,1]
-
-print(trap(h))
+    l,r = 0, len(height) - 1
+    left_max = height[l]
+    right_max = height[r]
+    res = 0
+    while l < r:
+        if left_max < right_max:
+            l+=1
+            left_max = max(left_max, height[l])
+            res += left_max - height[l]
+        else:
+            r -= 1
+            right_max = max(right_max, height[r])
+            res += right_max - height[r]
+    return res
