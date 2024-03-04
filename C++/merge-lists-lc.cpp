e72694dc19e6fb001f12b74643d5fd15a464a9ef
tyagidevansh <devanshtyagi0607@gmail.com>
2024-03-04 19:05:52 +0530

    usual

--- C++/merge-lists-lc.cpp
+++ C++/merge-lists-lc.cpp
@@ -1,26 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-    public:
-        ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-            if (!list1) return list2;
-            if (!list2) return list1;
+public:
+    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+        if (head1 == NULL) return head2;
+        if (head2 == NULL) return head1;
 
-            ListNode* dummy = new ListNode(-1);
-            ListNode* tail = dummy;
+        ListNode* dummy = new ListNode();
+        ListNode* head = dummy;
 
-            while (list1 && list2) {
-                if (list1->val <= list2->val) {
-                    tail->next = list1;
-                    list1 = list1->next;
-                } else {
-                    tail->next = list2;
-                    list2 = list2->next;
-                }
-                tail = tail->next;
+        while (head1 && head2) {
+            if (head1->val < head2->val) {
+                dummy->next = head1;
+                head1 = head1->next;
+            } else {
+                dummy->next = head2;
+                head2 = head2->next;
             }
-
-            if (list1) tail->next = list1;
-            if (list2) tail->next = list2;
-
-            return dummy->next;
+            dummy = dummy->next;
         }
+
+        if (head1) dummy -> next = head1;
+        if (head2) dummy -> next = head2;
+
+        return head->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) {
+            return NULL;
+        }
+        ListNode* head = lists[0];
+        
+        for (int i = 1; i < lists.size(); i++) {
+            head = mergeTwoLists(head, lists[i]);
+        }
+
+        return head;
+    }
 };
